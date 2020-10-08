ede7c0db6944fc0136a53ec1ca9210dedf54200e
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-10-08 22:31:40 +0100

    ADX-407 Test to check I havn't screwed any of the restricted fields up during refactoring.

--- tests/test_data_specs.py
+++ tests/test_data_specs.py
@@ -1,5 +1,6 @@
 import util
 import logging
+from pprint import pformat
 
 log = logging.getLogger(__name__)
 
@@ -21,3 +22,22 @@ def test_required_table_schemas_exist():
                         missing_table_schemas.append(field['field_value'])
                         log.error(f"Schema {value} referenced from {package} package not found.")
     assert missing_table_schemas == []
+
+
+def test_restricted_field_in_all_schemas():
+    package_schemas = util.package_schemas_generator()
+    for schema in package_schemas():
+        log.warning(pformat(schema.keys()))
+        resource_fields = schema['schema']['resource_fields']
+        restricted_fields = list(filter(
+            lambda x: x['field_name'] == 'restricted',
+            resource_fields
+        ))
+        assert len(restricted_fields) == 1
+        expected = {
+            "field_name": "restricted",
+            "label": "Access Restriction",
+            "preset": "restricted_fields",
+            "display_group": "Access Restriction"
+        }
+        assert restricted_fields[0] == expected
